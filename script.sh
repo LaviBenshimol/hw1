88f40eac800e867820363fd0ff9a73cda78255be
Lavi Benshimol <laviben@post.bgu.ac.il>
2020-04-09 17:16:21 +0300

    reinitiallized files

---
+++ script.sh
@@ -0,0 +1,56 @@
+parse_yaml() {
+    local yaml_file=$1
+    local prefix=$2
+    local s
+    local w
+    local fs
+
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_.-]*'
+    fs="$(echo @|tr @ '\034')"
+
+    (
+        sed -e '/- [^\“]'"[^\']"'.*: /s|\([ ]*\)- \([[:space:]]*\)|\1-\'$'\n''  \1\2|g' |
+
+        sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
+            -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
+            -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+            -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
+
+        awk -F"$fs" '{
+            indent = length($1)/2;
+            if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+            vname[indent] = $2;
+            for (i in vname) {if (i > indent) {delete vname[i]}}
+                if (length($3) > 0) {
+                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+                    printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+                }
+            }' |
+
+        sed -e 's/_=/+=/g' |
+
+        awk 'BEGIN {
+                FS="=";
+                OFS="="
+            }
+            /(-|\.).*=/ {
+                gsub("-|\\.", "_", $1)
+            }
+            { print }'
+    ) < "$yaml_file"
+}
+
+
+eval $(parse_yaml data.yml)
+echo 1
+ssh root@$VM_2 "iperf -s -p $A > server_output.txt &" 
+pid=$(ssh root@132.72.105.209 "pidof iperf")
+echo $pid
+iperf -c $VM_2 $SEND_TYPE -p $A -t $B -i $C > $MY_FILE
+ssh root@$VM_2 "kill \"$pid\""
+#ssh root@$VM_2 "kill \"$pid\" | tee out3 "
+
+#ssh root@$VM_2 "echo $!"
+#ssh root@$VM_2 "ifconfig"
+
